ee3f648548b9ccdffabe3763618d41eb2a99c9e8
xixi <github@firadio.com>
2025-01-14 16:17:48 +0800

    add config

--- src/index.tsx
+++ src/index.tsx
@@ -2,10 +2,11 @@
 import type React from 'react';
 import ReactDOM from 'react-dom/client';
 import RouteIndex from './routes/index';
+import { config } from './config';
 
 // 修改 root 元素的 CSS
 const rootElement = document.getElementById('root');
 if (rootElement) {
 	rootElement.style.height = '100%';
-	ReactDOM.createRoot(rootElement).render(<RouteIndex />);
+	ReactDOM.createRoot(rootElement).render(<RouteIndex config={config} />);
 }
